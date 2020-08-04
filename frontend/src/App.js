c23ab67374c551258c7afd37098247562470535a
JaviFdezC <46001349+JaviFdezC@users.noreply.github.com>
2020-08-04 13:18:03 +0200

    components creation and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navigation from './components/Navigation'
+import ContactsList from './components/ContactsList'
+import CreateContact from './components/CreateContact'
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Router>
+      <Navigation/>
+      
+      <Route path='/' exact component={ContactsList}/>
+      <Route path='/admin' component={CreateContact}/>
+      <Route path='/edit/:id' component={CreateContact}/>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/ContactsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContactsList extends Component {
+    render() {
+        return (
+            <div>
+                Contacts List
+            </div>
+        )
+    }
+}
